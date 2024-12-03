9f9afc76bd3a88ca2661d9f151c99ffdb0c4876b
marcela-c-oliveira <marcela.mah2010@gmail.com>
2024-12-03 11:42:52 -0300

    Alterei código e atualizei apresentação

--- Trabalho_FMC.R
+++ Trabalho_FMC.R
@@ -107,7 +107,7 @@ ggplot(dfw,aes(y=UPAPS,x=Categoria..Experient.ou.Student.,fill=Categoria..Experi
   geom_boxplot(position=position_dodge(.1),outlier.shape = NA)+
   geom_point(position=position_jitter(width=.15,height=.2),size=2.5,shape=16,alpha=0.7,color="gray10")+
   stat_summary(fun=mean,geom="point",size=7,shape=23,color="black",fill = rgb(0, 0, 0, alpha = 0.4),position=position_dodge(0.1))+
-  geom_hline(yintercept=4, color="gray20", linewidth=0.8)+
+  geom_hline(linetype="dashed",yintercept=4, color="gray20", linewidth=0.8)+
   facet_wrap(~Timepoint) +
   labs(title = "Pontuação da dor em porcos antes e após castração",
        x = "Avaliadores",
@@ -141,6 +141,7 @@ ggplot(data = prop, aes(x = factor(Categoria), y = Analgesia, fill = Categoria))
   geom_bar(stat = "identity", position = "dodge") +
   geom_text(aes(label = scales::percent(Analgesia / 100)), position = position_dodge(width = 0.9), vjust = -0.3, size = 4.7) +
   geom_text(data = subset(prop, Categoria == "Experientes" & Timepoint == "Depois da castração"), aes(x = factor(Categoria), y = Analgesia, label = "*"), position = position_dodge(width = 0.9), vjust = -.8, size = 10, inherit.aes = FALSE) +
+  geom_text(data = subset(prop, Categoria == "Experientes" & Timepoint == "Antes da castração"), aes(x = factor(Categoria), y = Analgesia, label = "*"), position = position_dodge(width = 0.9), vjust = -.8, size = 10, inherit.aes = FALSE) +
   facet_wrap(~ Timepoint) +
   labs(
     title = "Indicação analgésica em porcos antes e após castração",
@@ -156,3 +157,37 @@ ggplot(data = prop, aes(x = factor(Categoria), y = Analgesia, fill = Categoria))
                         plot.margin = unit(c(.3,.3,.3,.3),"cm"),
                         legend.position = 'none')
 dev.off()
+
+
+# Matrix with expected values - before castration ####
+
+df_c0$Analgesia <- factor(df_c0$Analgesia, 
+                          levels = c("0", "1"), 
+                          labels = c("No analgesia", "Analgesia"))
+
+table(df_c0$Categoria..Experient.ou.Student.,df_c0$Analgesia)
+
+value11 <- (40 + 67) * (40 + 0) / 140
+value12 <- (33 + 0) * (40 + 0) / 140
+value21 <- (40 + 67) * (67 + 33) / 140
+value22 <- (33 + 0) * (67 + 33) / 140
+
+expected <- matrix(
+  round(c(value11, value12, value21, value22), 1), 
+  nrow = 2, 
+  byrow = TRUE,
+  dimnames = list(
+    c("Experients", "Students"), 
+    c("No analgesia", "Analgesia")
+  )
+)
+
+print(expected)
+
+# Como a tabela de valores esperados nao apresentou nenhum numero menor que 5, podemos usar o teste de qui quadrado, nao e necessario/obrigatorio usar fisher
+
+# Chi square test OR Fisher test - before castration ####
+
+chisq.test(table(df_c0$Categoria..Experient.ou.Student.,df_c0$Analgesia)) # p-value = 0.002309
+
+fisher.test(table(df_c0$Categoria..Experient.ou.Student.,df_c0$Analgesia)) # p-value = 0.001101
